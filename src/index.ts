df598a9c1e56af4f381e1744becdb07916e37fab
sinlc <56957789+sinlc@users.noreply.github.com>
2026-09-11 21:06:19 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export default {
 		/**
 		 * Replace `remote` with the host you wish to send requests to
 		 */
-		const remote = "https://example.com";
+		const remote = "https://sites.google.com/view/sinlc";
 
 		return await fetch(remote, request);
 	},
